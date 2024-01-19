dfe17146e3ff0804d5e50be49c3539ecc3a1f5f4
HduSy <hdusy14032122@gmail.com>
2024-01-19 19:49:25 +0800

    chore: 优化 useCountDown 实现

--- README.md
+++ README.md
@@ -7,15 +7,20 @@ pnpm add oh-my-vueuse --save
 ```
 ## Hooks
 ### useCountDown
-倒计时 Hook
+倒计时 Hook ，
+#### 特征
+利用 `requestAnimationFrame` 实现**高性能、精准**倒计时，分别体现在：
+- 性能比较：页面不可见时，暂停倒计时代码的执行 `raf > setTimeout > setInterval`；
+- 精准度：依赖于系统刷新率，时间间隔固定;
+- 兼容性；[requestAnimationFrame | Can I use](https://caniuse.com/?search=requestAnimationFrame)
 ```ts
 import { useCountDown } from 'oh-my-vueuse'
 
 // time Ref<number>, setTime (t: number) => void
 const [time, setTime] = useCountDown(5 * 1000)
 
-// time: 剩余时间
-// setTime: 传入一个新的时间，重新倒计时
+// timestamp: 剩余时间(ms时间戳格式)
+// setTime: 传入一个新的时间戳，重新开始倒计时
 ```
 ### 建设中... 🚜
 ...
--- src/useCountDown.ts
+++ src/useCountDown.ts
@@ -1,42 +1,45 @@
 import { onMounted, onUnmounted, ref, Ref } from 'vue'
 
-export default function useCountDown(lastTime: number): [Ref<number>, (time: number) => void] {
-  const time = ref(lastTime) // 剩余时间
+export default function useCountDown(timestamp: number): [time: Ref<number>, setTime: (time: number) => void] {
+  const lastTime = ref(timestamp) // 剩余时间
   // 重设时间计时 ⌛️
-  const setTime = (t: number) => {
-    timer.value && clearTimeout(timer.value)
+  const setTime = (timestamp: number) => {
+    cancelAnimationFrame(timer.value)
     startTime.value = Date.now()
-    time.value = t
-    calcDown()
+    lastTime.value = timestamp
+    loop()
   }
-  
-  const timer = ref(0) // setTimeout 计时器
-  const startTime = ref(Date.now())
-  const count = ref(0) // 递归次数
-  const interval = ref(1000) // 时间间隔
+
+  const timer = ref(0) // requestAnimationFrame 计时器
+  const startTime = ref(Date.now()) // 开始时间 !important
+  const interval = ref(1000) // 时间间隔 1s
+  const past = ref(0) // 经过时间
 
   const calcDown = () => {
-    time.value -= interval.value
-    // 置为 0
-    if(time.value < 0) {
-      time.value = 0
-      clearTimeout(timer.value)
-      return
+    startTime.value = Date.now() // 更新开始时间
+    lastTime.value = Math.round((lastTime.value - past.value)/1000) * 1000
+    if(lastTime.value <= 0) {
+      // 倒计时终止
+      cancelAnimationFrame(timer.value)
+      lastTime.value = 0
     }
-    let offset = Date.now() - (startTime.value + count.value * interval.value) // 偏差
-    if(offset < 0) offset = 0
-    const nextDelay = interval.value - offset // 修正后间隔
-    count.value = count.value + 1
-    console.log("误差：" + offset + "ms，下一次执行：" + nextDelay + "ms后，离活动开始还有：" + time.value + "ms");
-    timer.value = setTimeout(calcDown, nextDelay)
   }
-  
+  // raf > setTimeout > setInterval
+  const loop = () => {
+    let now = Date.now()
+    past.value = now - startTime.value
+    if(past.value >= interval.value) {
+      calcDown()
+    }
+    timer.value = requestAnimationFrame(loop)
+  }
+
   onMounted(() => {
-    calcDown()
+    loop()
   })
   onUnmounted(() => {
-    clearTimeout(timer.value)
+    cancelAnimationFrame(timer.value)
   })
 
-  return [ time, setTime ]
+  return [lastTime, setTime]
 }
